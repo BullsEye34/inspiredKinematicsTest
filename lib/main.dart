883aae15769c14c1362e1482f5181440e93e2357
Vamshi Prasad <vamvijkum@gmail.com>
2020-05-23 17:08:02 +0530

    Make stack and add container

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
 void main() {
-  runApp(app());
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: app(),
+    ),
+  );
 }
 
 class app extends StatefulWidget {
@@ -13,6 +18,28 @@ class app extends StatefulWidget {
 class _appState extends State<app> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var h = MediaQuery.of(context).size.height;
+    var w = MediaQuery.of(context).size.width;
+    print(h);
+    return SafeArea(
+      child: Container(
+        color: Color(0x111f1b24), // Kinda grey Color
+        child: Stack(
+          // Thinking of Stacking up image and text and other stuff
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 15, // 15 means 45px
+                vertical: 13.333, // 13.333 menas 40px
+              ),
+              child: Container(
+                color: Colors.white,
+                height: h / 1.5,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
