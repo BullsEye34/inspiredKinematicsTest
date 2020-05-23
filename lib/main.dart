10ba004c86a42188458a9421c746eeb1391684ee
Vamshi Prasad <vamvijkum@gmail.com>
2020-05-23 16:29:09 +0530

    Make a basic APP

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(app());
+}
+
+class app extends StatefulWidget {
+  @override
+  _appState createState() => _appState();
+}
+
+class _appState extends State<app> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
