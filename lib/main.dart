56da51ab55ed5e187f25a1b91e9a69eaed354223
Vamshi Prasad <vamvijkum@gmail.com>
2020-05-23 17:26:15 +0530

    Add json

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(
@@ -11,17 +12,70 @@ void main() {
   );
 }
 
+class User {
+  var product_name;
+  var description;
+  var images;
+
+  User(var product_name, var description, var images) {
+    this.product_name = product_name;
+    this.description = description;
+    this.images = images;
+  }
+
+  User.fromJson(Map json)
+      : product_name = json['product_name'],
+        description = json['description'],
+        images = json['images'];
+
+  Map toJson() {
+    return {
+      'product_name': product_name,
+      'description': description,
+      'images': images,
+    };
+  }
+}
+
 class app extends StatefulWidget {
   @override
   _appState createState() => _appState();
 }
 
+class API {
+  static Future getUsers() {
+    var url =
+        "https://drive.google.com/file/d/1LrOl1HcSmqDhk5nF40KwuO_IwlaJyK2X/view?usp=sharing";
+    print(http.get(url));
+    return http.get(url);
+  }
+}
+
 class _appState extends State<app> {
+  var users = new List<User>();
+  _getUsers() {
+    API.getUsers().then((response) {
+      var o = response;
+      print(o.body.toString());
+      o = o.body.toString().substring(3);
+      setState(() {
+        Iterable list = json.decode(o);
+        users = list.map((model) => User.fromJson(model)).toList();
+      });
+    });
+  }
+
+  initState() {
+    super.initState();
+    _getUsers();
+  }
+
   @override
   Widget build(BuildContext context) {
     var h = MediaQuery.of(context).size.height;
     var w = MediaQuery.of(context).size.width;
-    print(h);
+    //print(h);
+
     return SafeArea(
       child: Container(
         color: Color(0x111f1b24), // Kinda grey Color
